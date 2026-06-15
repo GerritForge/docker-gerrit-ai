27abb67a641ba1cd74b780ef7b9a81b91a8bfc32
Luca Milanesio <luca.milanesio@gmail.com>
2026-06-15 19:14:30 +0100

    Set serverId matching the sample repository
    Change-Id: Ibe9d2a7d9f8ea38b7bdfe347ee3ba48b39b28071

--- gerrit-ai-entrypoint.sh
+++ gerrit-ai-entrypoint.sh
@@ -5,5 +5,6 @@ setConfig() {
 }
 
 setConfig httpd.firstTimeRedirectUrl /login/%23%2Fstatic%2Fintro.html?account_id=1000000
+setConfig gerrit.serverId 4ca5675a-8812-4c33-b94f-ce3de104a853
 
 exec /entrypoint.sh
